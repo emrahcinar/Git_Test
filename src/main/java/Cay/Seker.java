7df82ce928e25e1606965439d45f96319990535f
emrahcinar <emrah-c@hotmail.com>
2020-08-23 23:38:51 -0400

    Cift

--- src/main/java/Cay/Seker.java
+++ src/main/java/Cay/Seker.java
@@ -3,9 +3,8 @@ package Cay;
 public class Seker {
 
     public static void main(String[] args) {
-        System.out.println("Tek seker");
-        System.out.println("Tek seker");
-        System.out.println("Tek seker");
-        System.out.println("Tek seker");
+        System.out.println("Cift seker");
+
+
     }
 }
