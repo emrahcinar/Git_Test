79ca2515b558bad4a5687816b34a5d0d032c41cb
emrahcinar <emrah-c@hotmail.com>
2020-08-24 11:39:11 -0400

    az sekerli

--- src/main/java/Cay/Seker.java
+++ src/main/java/Cay/Seker.java
@@ -4,9 +4,8 @@ public class Seker {
 
     public static void main(String[] args) {
 
-        System.out.println("Cift seker");
+        System.out.println("Cok az seker");
       
-        System.out.println("Cift sekerli cay");
 
     }
 }
