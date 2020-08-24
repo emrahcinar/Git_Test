62d89afcd068b9188c07e753c74825de2955c38b
emrahcinar <emrah-c@hotmail.com>
2020-08-23 23:40:11 -0400

    cok seker

--- src/main/java/Cay/Seker.java
+++ src/main/java/Cay/Seker.java
@@ -3,9 +3,7 @@ package Cay;
 public class Seker {
 
     public static void main(String[] args) {
-        System.out.println("Tek seker");
-        System.out.println("Tek seker");
-        System.out.println("Tek seker");
-        System.out.println("Tek seker");
+        System.out.println("Cok seker");
+
     }
 }
